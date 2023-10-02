093f1bc9769214a26c3d660bdbbee1cf4477d450
Esmuellert1325 <faanandor@gmail.com>
2023-10-02 16:31:01 +0200

    Added image to boxes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import service as Service
 
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
 window = tk.Tk()
 window.geometry("1000x650")
 window.title("Youtube Music Downloader")
@@ -19,14 +23,27 @@ def update_button_text(event=None):
 def create_video_box(video):
     video_frame = tk.Frame(container, bd=1, bg="white", padx=10, pady=10)
     video_frame.pack(pady=5, fill=tk.X)
-
-    #label = tk.Label(video_frame, image=video['url'])
+    #label = tk.Label(video_frame, image=video['cover'])
     #label.pack(side=tk.LEFT)
-    
+    response = requests.get(video['cover'])
+    image_data = response.content
+
+    image = Image.open(BytesIO(image_data))
+
+    desired_width = 200  # Adjust this to your preferred width
+    desired_height = 150  # Adjust this to your preferred height
+    image.thumbnail((desired_width, desired_height))
+
+    photo = ImageTk.PhotoImage(image)
+
+    image_label = tk.Label(video_frame, image=photo)
+    image_label.image = photo
+    image_label.pack()
+
     title_label = tk.Label(video_frame, text=video['title'])
     title_label.pack(side=tk.LEFT, padx=10)
     
-    download_button = tk.Button(video_frame, text="Download", command=lambda: download_video(video['url']))
+    download_button = tk.Button(video_frame, text="DOWNLOAD", command=lambda: download_video(video['url']), bg="red", fg="white")
     download_button.pack(side=tk.RIGHT)
 
 def add_videos_to_container():
@@ -79,8 +96,19 @@ scrollbar.pack(side="right", fill="y")
 canvas.configure(yscrollcommand=scrollbar.set)
 
 container = tk.Frame(canvas)
-canvas.create_window((0, 0), window=container, anchor="nw")
+canvas.create_window((canvas.winfo_width() / 2, 0), window=container, anchor="n")
 
-container.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+def on_mousewheel(event):
+    canvas.yview_scroll(-1 * (event.delta // 120), "units")
+
+canvas.bind_all("<MouseWheel>", on_mousewheel)
+
+# Configure the canvas to update the scrollregion when the container changes size
+def configure_scroll_region(event):
+    canvas.config(scrollregion=canvas.bbox("all"))
+
+container.bind("<Configure>", configure_scroll_region)
+
+#container.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
 window.mainloop()
